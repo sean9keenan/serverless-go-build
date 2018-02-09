fced3f65b4a3f28cc7a47f5865b68751c0d07bdc
sean9keenan <sean9keenan@gmail.com>
2018-02-09 03:07:55 -0800

    initial commitz

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # serverless-go-build
 Makes building go with serverless easy!
+
+ - `serverless build` : Builds _all_ Go binaries listed as function handlers
+ - `serverless test` : Allows running tests
+ - Heavy customization : (Doesn't even need to build Go!)
+
+`serverless deploy` will not run the builds - run `serverless build` first.
+
+Will support the following very soon:
+ - Allow packageName.FunctionName as function handlers
+ - Automatically packaging go binaries into separate deployed zips
+ - Allowing running serverless dev environments when running tests (eg: dynamoDB)
+ - `test` command supporting 
+
+Example `serverless.yml`
+```yaml
+service: myService
+plugins:
+  - serverless-go-build
+custom:
+  go-build:
+    tests:
+      - ./endpoints
+package:
+  exclude:
+    - ./**
+  include:
+    - ./bin/**
+functions:
+  getWidget:
+    handler: entrypoints/widget/get.go
+    name: myService-${self:provider.stage}-getWidget
+    events:
+      - http:
+          path: widget
+          method: get
+  postWidget:
+    handler: entrypoints/widget/post.go
+    name: myService-${self:provider.stage}-postWidget
+    events:
+      - http:
+          path: widget
+          method: post
+```
+
+## Customization
+The most up-to-date place to look is the code - the top of `index.js`.
+
+ - Prefix used for building for AWS
+    + `awsbuildPrefix: 'GOOS=linux '`
+ - Build command - followed by bin dest and input path
+    + `buildCmd: 'go build -ldflags="-s -w" -o'`
+ - Test command - followed by value in tests array below
+    + `testCmd: 'GO_TEST=serverless go test'`
+ - Path to store build results
+    + `binPath: 'bin/',`
+ - Array of tests to run
+    + `tests: []`
+
---
+++ index.js
@@ -0,0 +1,171 @@
+'use strict';
+
+const BbPromise = require('bluebird');
+const chalk = require('chalk');
+const execPromise = require('./lib/execPromise');
+
+/**
+ * Default values for the serverless.yml definition.
+ *
+ * You can override any of these by overriding them in custom.go-build
+ * @type {Object}
+ */
+const defaultGoDict = {
+  // Prefix used for building for AWS
+  awsbuildPrefix: 'GOOS=linux ',
+  // Build command - followed by bin dest and input path
+  buildCmd: `go build -ldflags="-s -w" -o`,
+  // Test command - followed by value in tests array below
+  testCmd: `GO_TEST=serverless go test`,
+  // Path to store build results
+  binPath: 'bin/',
+  // Array of tests to run
+  tests: [],
+}
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.commands = {
+      build: {
+        usage: 'Builds your go files needed for deployment',
+        lifecycleEvents: [
+          'build',
+        ],
+        options: {
+          local: {
+            usage:
+              'If the build should be made for the local machine (otherwise defaults to AWS deployment)',
+            shortcut: 'l',
+          },
+        },
+      },
+      test: {
+        usage: 'Runs your go tests',
+        lifecycleEvents: [
+          'test',
+        ],
+      },
+    };
+
+    this.hooks = {
+      'build:build': this.build.bind(this),
+      'test:test': this.tests.bind(this),
+      'before:deploy:function:packageFunction': this.predeploy.bind(this),
+      'before:package:createDeploymentArtifacts': this.predeploy.bind(this),
+    };
+  }
+
+  getGoConfigParam(param) {
+    try {
+      const val = this.serverless.service.custom['go-build'][param]
+      return val !== undefined ? val : defaultGoDict[param]
+    }
+    catch (err) {
+      return defaultGoDict[param]
+    }
+  }
+
+  getRelevantGoFunctions() {
+    let functionNames;
+    
+    if (this.options.function) {
+      functionNames = [this.options.function];
+    } else {
+      // Get all functions just gets names - not full object
+      functionNames = this.serverless.service.getAllFunctions()
+    }
+
+    // Retrieve the full objects
+    const functions = functionNames.map((func) => this.serverless.service.getFunction(func))
+
+    // Filter out functions that are not golang
+
+    // First determine if project default is golang
+    const isProjectGolang = serverless.service.provider.runtime === "go1.x"
+
+    let isFileGolangFunc;
+    if (isProjectGolang) {
+      isFileGolangFunc = f => !f.runtime || f.runtime === "go1.x"
+    } else {
+      isFileGolangFunc = f => f.runtime && f.runtime === "go1.x"
+    }
+
+    const goFunctions = functions.filter(isFileGolangFunc)
+
+    return goFunctions
+  }
+
+  getOutputBin(func) {
+      let outputbin = func.handler.replace(/\.go$/, "")
+      const binPath = this.getGoConfigParam('binPath')
+      outputbin = binPath + outputbin
+      return outputbin
+  }
+
+
+  build() {
+    this.serverless.cli.log('Beginning Go build');
+
+    // Run build on relevant go functions
+    const functions = this.getRelevantGoFunctions();
+
+    return BbPromise.mapSeries(functions, func => {
+
+      // Construct the build command
+      const awsbuildPrefix = this.getGoConfigParam('awsbuildPrefix')
+      const buildPrefix = awsbuildPrefix + this.getGoConfigParam('buildCmd')
+      const buildCmd = `${buildPrefix} ${this.getOutputBin(func)} ${func.handler}`
+
+      // Log the build command being run
+      this.serverless.cli.log(buildCmd);
+
+      // Return a promise executing the build command
+      return execPromise(buildCmd).catch(err => {
+        // Couldn't build file - output appropriate errors
+        console.log(chalk.red(`Error building golang file at ${func.handler}\n` + 
+                              `To replicate please run:\n` + 
+                              `${buildCmd}\n`));
+        throw Error("Go build failure")
+      });
+    });
+  }
+
+  tests() {
+    this.serverless.cli.log('Running Go tests')
+
+    const tests = this.getGoConfigParam('tests')
+
+    if (!tests.length) {
+      console.log(chalk.red('No tests to run - add tests to custom.go-build.tests in your serverless file.'))
+    }
+
+    return BbPromise.mapSeries(tests, test => {
+      // Construct the test command
+      const testPrefix = this.getGoConfigParam('testCmd')
+      const testCmd = `${testPrefix} ${test}`
+
+      // Return a promise executing the build command
+      return execPromise(testCmd).catch(err => {
+        // Couldn't build file - output appropriate errors
+        console.log(chalk.red(`Error running test on ${test}\n` + 
+                              `To replicate please run:\n` + 
+                              `${testCmd}\n`));
+        throw Error("Go test failure")
+      });
+    })
+  }
+
+  predeploy() {
+    this.serverless.cli.log(`Reassigning go paths to point to ${this.getGoConfigParam('binPath')}`);
+
+    const functions = this.getRelevantGoFunctions();
+    for (const func of functions) {
+      func.handler = this.getOutputBin(func)
+    }
+  }
+}
+
+module.exports = ServerlessPlugin;
